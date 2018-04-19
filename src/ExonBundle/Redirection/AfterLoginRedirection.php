513be126383286eae86d09c067f09cf07b6dc042
kais selmi <selmi.kaies@gmail.com>
2018-04-19 11:20:03 +0100

    bundle

---
+++ src/ExonBundle/Redirection/AfterLoginRedirection.php
@@ -0,0 +1,48 @@
+<?php
+
+
+namespace ExonBundle\Redirection;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
+
+class AfterLoginRedirection implements AuthenticationSuccessHandlerInterface
+{
+    /**
+     * @var \Symfony\Component\Routing\RouterInterface
+     */
+    private $router;
+
+    /**
+     * @param RouterInterface $router
+     */
+    public function __construct(RouterInterface $router)
+    {
+        $this->router = $router;
+    }
+
+    /**
+     * @param Request $request
+     * @param TokenInterface $token
+     * @return RedirectResponse
+     */
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token)
+    {
+
+        $roles = $token->getRoles();
+
+        $rolesTab = array_map(function($role){
+            return $role->getRole();
+        }, $roles);
+
+        if (in_array('ROLE_ADMIN', $rolesTab, true) || in_array('ROLE_SUPER_ADMIN', $rolesTab, true))
+            $redirection = new RedirectResponse($this->router->generate('admin_homepage'));
+        else
+            $redirection = new RedirectResponse($this->router->generate('user_homepage'));
+
+        return $redirection;
+    }
+}
